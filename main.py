31786388b72b3a51e45ee0d80cc833bc444fdb33
Kaan <drbot7@gmail.com>
2022-04-29 03:00:58 -0400

    3d maps

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ data = json.load(f)
 keys = data.keys()
 x = []
 y = []
+z = []
 names = []
 arrows = []
 for key in keys:
@@ -17,19 +18,23 @@ for key in keys:
     person = data[key]
     x.append(person['x'])
     y.append(person['y'])
+    z.append(person['z'])
     names.append(key)
     children = person['children']
     for i, child in enumerate(children):
         arrows.append([person['x'], person['y'], person['children'][i]['x']-person['x'], person['children'][i]['y']-person['y']])
 
-plt.figure(figsize=(8, 8))
-plt.scatter(x, y, s=1, color="red")
+fig = plt.figure(figsize=(8, 8))
+ax = fig.add_subplot(projection='3d')
+
+ax.scatter(x, y, z, s=2, color="red")
 plt.xlabel("DFS Topological Sort")
 plt.ylabel("Kahn's Algorithm Topological Sort")
+ax.set_zlabel("Inverse DFS Topological Sort")
 plt.title("Royal Family Tree", fontsize=15)
-for i, label in enumerate(names):
-    plt.annotate(label, (x[i], y[i]), fontsize=3)
-for row in arrows:
-    plt.quiver(row[0], row[1], row[2], row[3], angles='xy', scale_units='xy', scale=1, width=0.0001)
+#for i, label in enumerate(names):
+#    plt.annotate(label, (x[i], y[i]), fontsize=5)
+#for row in arrows:
+#    plt.quiver(row[0], row[1], row[2], row[3], angles='xy', scale_units='xy', scale=1, width=0.001)
 plt.show()
 f.close()
